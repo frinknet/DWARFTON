a6ee561c540066958d7bb65c4fcfd300664b14ac
Micon Frink <frink@frinknet.com>
2018-03-05 00:28:07 -0500

    change guild slightly without changing output

--- utils/build.sh
+++ utils/build.sh
@@ -5,19 +5,15 @@ shift
 
 line() { echo $1 >> $OUT; }
 name() { basename $1 .js | tr '[a-z]' '[A-Z]' | sed 's/[^a-z]+/_/g'; }
-check() { [ ! -f "$1" ] && echo "Missing $1 file." && exit; }
 
-check COPYRIGHT
-check VERSION
-
-echo "/*©$(cat COPYRIGHT)*/" > $OUT
+[ -f COPYRIGHT ] && echo "/*©$(cat COPYRIGHT)*/" > $OUT|| echo > $OUT
 
 line '"use strict"'
 line
-line "const $(name $OUT)=$(cat VERSION)"
+
+[ -f VERSION ] && line "const $(name $OUT)=$(cat VERSION)"
 
 for x in $@;do
-  check $x
   line "/*$(name $x)*/"
   cat "$x" >> $OUT
   line
