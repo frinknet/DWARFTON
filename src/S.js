97ee33f6fe6b5e1929314673cdaddf06b4dfd9d5
M. Frink ~ Lemur <frink@frinknet.com>
2021-08-19 22:10:16 -0500

    Update S.js

--- src/S.js
+++ src/S.js
@@ -2,9 +2,14 @@
 //k=key
 //v=value
 S=(k,v,s)=>(
+	//choose whether session or local storage
 	s=k[0]=='*'?W.sessionStorage:W.localStorage,
+	D?
+	//check if we have a value
 	v==U
+	//if not return the value
 	?s.getItem(k)
+	// 
 	:s.setItem(k,v)
 	:U
 ),
