c998241f205298413489c519477651b0c079b88c
M. Frink ~ Lemur <frink@frinknet.com>
2021-08-19 21:08:25 -0500

    shortening

--- src/P.js
+++ src/P.js
@@ -1,8 +1,4 @@
 //Prototype
 //o=object
 //a=alternative
-P=function(o,a){
-	var o=Object(o)
-	
-	return a?o[p]=P(a):o.prototype||o.__proto__
-},
+P=(o,a)=>(o=O(o),a?o[p]=P(a):o.prototype||o.__proto__),
