f6c0952baa952df0b68c25c872290e6253ac55f6
M. Frink ~ Lemur <frink@frinknet.com>
2021-08-20 16:33:56 -0500

    shortening more

--- src/O.js
+++ src/O.js
@@ -1,7 +1,7 @@
 //Objectify
 // o=object
 // a=assignments
-O=(...a)=>Object.assign(
+O=(...a)=>Object.assign({},
 	//filter objects to avoid errors and insure we always return an object
-	...[...a,{}].filter(o=>o!=U&&o!=N&&Object(o))
+	...a.filter(o=>o!=U&&o!=N&&Object(o))
 )
