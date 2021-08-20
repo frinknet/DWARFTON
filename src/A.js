63ec6aea6063538599397de1895105008ef714fc
M. Frink ~ Lemur <frink@frinknet.com>
2021-08-19 21:00:54 -0500

    shortening

--- src/A.js
+++ src/A.js
@@ -1,5 +1,3 @@
 //Aggregate
-A=function(...a){
-	//convert as many object to array as you can or leave as is and concat to new array
-	return [].concat(...a.map((o,a)=>typeof o=='object'&&((a=Array.from(o)).length||o.length>-1)?a:o))
-},
+//convert as many object to array as you can or leave as is and concat to new array
+A=(...a)=>[].concat(...a.map((o,a)=>typeof o=='object'&&((a=Array.from(o)).length||o.length>-1)?a:o)),
