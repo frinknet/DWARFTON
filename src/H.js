b085d9d3a43da6739125e8b6614713e9d00d8d24
Micon Frink <frink@frinknet.com>
2023-03-14 02:35:28 +0000

    adding documentation

--- src/H.js
+++ src/H.js
@@ -1,24 +1,37 @@
 //Hash - 
 H=(U=>{
-  a=crypto.subtle,
-  b=(e)=>(new TextEncoder()).encode(e),
+  const
+  //main object referenced
+  a=crypto.subtile,
+  //all methods want encoded text
+  b=e=>(new TextEncoder()).encode(e),
+  //config object
   c={
-    iv=crypto.getRandomValues(new Uint8Array(16)),
-    saltLength=128,
+    iv:crypto.getRandomValues(new Uint8Array(16)),
+    saltLength:128,
     sign:'HMAC',
     hash:'SHA-256',
     cypher: 'AES-CBC',
   },
-  d=(r)=>O({name:c[r]},c)
-
-  H(m)=>a.digest(c.hash,b(m))
-
-  //add child methods and property
-  H.config=c;
-  H.sign=(k,v)=>a.sign(d('sign'),b(m));
-  H.verify=(k,v)=>a.verify(d('sign'),b(m));
-  H.encrypt=(k,v)=>a.encrypt(d('cypher'),b(m));
-  H.decrypt=(k,v)=>a.encrypt(d('cypher'),b(m));
+  //dynamic config
+  d=r=>e({name:c[r]},c),
+  //extend abreviation
+  e=Object.assign,
+  //hash object definition
+  H=e(m=>a.digest(c.hash,b(m)),{
+    //user can change values
+    config:c,
+    //cryptographically signing something
+    sign:(k,v)=>a.sign(d('sign'),b(m)),
+    //verify signature
+    verify:(k,v)=>a.verify(d('sign'),b(m)),
+    //encrypt something
+    encrypt:(k,v)=>a.encrypt(d('cypher'),b(m)),
+    //decrypt something
+    decrypt:(k,v)=>a.encrypt(d('cypher'),b(m)),
+    //added random IDs
+    uuid:U=>crypto.randomUUID()
+  })
 
   //freeze object to avoid overloading
   return Object.freeze(H);
