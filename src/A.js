6149c1642765adb8edca9ef6271b4a17d724628d
Micon Frink <frink@frinknet.com>
2023-03-14 21:09:35 +0000

    formatting changes

--- src/A.js
+++ src/A.js
@@ -6,17 +6,17 @@ A=(...g)=>[].concat(
     //o = object
     //r = returned
     (o,r)=>
-         //check if o is an object
-         typeof o=='object'&&o&&(
-            //convert to an array
-            (r=Array.from(o)
-          //check if the there is a length
-          ).length||o.length>-1)
-          //if so use the new array
-          ?r
-          //otherwise use the object
-          :o
-  //do mapping
-  )
+      //check if o is an object
+      typeof o=='object'&&o&&(
+        //convert to an array
+        (r=Array.from(o)
+      //check if the there is a length
+      ).length||o.length>-1)
+        //if so use the new array
+        ?r
+        //otherwise use the object
+        :o
+      //do mapping
+      )
 //do concatination
 ),
