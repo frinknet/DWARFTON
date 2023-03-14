62a75cf497d91771c5a0d2ef882aadf3060cad81
Micon Frink <frink@frinknet.com>
2023-03-14 21:15:39 +0000

    consolidating shaving bytes

--- src/O.js
+++ src/O.js
@@ -1,54 +1,44 @@
 //Overloader - O(...l)
 //l=list of object
-O=((v,e,r,l,o,a,d)=>{
-  //label maker
-  l=n=>'D:'+n[r](
+O=((v,e,r,l,o,a,d)=>(
+  //assign
+  a=v.assign,
+  //dynamic name
+  d=n=>'D:'+n[r](
       //replace first letters
       /(?:^\w|[A-Z]|\b\w)/g,
       //set to uppercase
       m=>m.toUpperCase()
   //replace non-letters with nothing
   )[r](/[^A-Za-z]+/g,''),
-  //register object
-  o=(c,p)=>(
-    //save parent in extensions table
-    a(e,eval(`({'${c}:parent':d(p)})`)),
-    //save class in extensions table
-    a(e,eval(`({'${c}':class extends e['${c}:parent']{}})`))
-  ),
-  //abbreviate assignments
-  a=v.assign,
-  //describe
-  d=o=>(
-    //if this is a function extend from there
-    (o.call&&o)||
-    //otherwise if it it is a prototype extend there
-    (o.prototype||v.getPrototypeOf(o)).constructor
-    )
-
   //return a new Proxy
-  return new Proxy(
+  new Proxy(
     //return the default action
-    (...l)=>a({},...l.filter(o=>o&&!o.trim&&v(o))),
+    (...l)=>a({},...l.filter(s=>s&&!s.trim&&v(s))),
     //now overload with proxy
     {
       //set the prototype on new classes
-      set:(_,n,s)=>{
-        //set child label
-        n=l(n)
-        //make sure we arent doing this twice
-        if(e[n])throw 'Class already exists'
-        //see if we can set the class
-        o(n,s)
-      },
+      set:(_,n,s)=>(
+        //save parent in extensions table
+        e[n=d(n)]||(
+          //if this is a function extend from there
+          e[n+':def']=(s.call&&s)||
+          //otherwise extend the prototype
+          (s[l]||v.getPrototypeOf(s))[o],
+          //save class in extensions table
+          a(e,eval(`({'${n}':class extends e['${n}:def']{}})`))
+        ),
+        a(e[n][l],s[l]||s)
+      ),
       //return a method to create a class
-      get:(_,n)=>e[l(n)]&&function(...a) {
+      get:(_,n)=>e[n=d(n)]&&a(function(...a){
         //run with arguments
-        return new (e[l(n)])(...a)
-      }
+        return new (e[n])(...a)
+      //return function with prototype and constructor
+      },{[o]:e[n],[l]:e[n][l]})
     //that's all folks
     }
   //return the proxy
   )
-//send in abreviations
-})(Object,new Map,'replace'),
+//send abreviations v,e,r,l,o
+))(Object,{},'replace','prototype','constructor'),
